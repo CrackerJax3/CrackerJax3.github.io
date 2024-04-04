bf3496e6a7970ef3fba6ca0f715bf01d87358eae
Crackerjax3 <jax.yuen@gmail.com>
2024-04-04 16:01:24 +0300

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Header.css'; // Import your CSS file for styling
+// import './Header.css'; // Import your CSS file for styling
 import './App.css'; // You can create this file for styling
 import Slideshow from './Slideshow';
 import logo from './msxbigwhitetransparent.png'; // Import your logo image file
-import './Footer.css';
+// import './Footer.css';
 import Sidebar from './Sidebar'; // Import the Sidebar component
-// add this in the right place
-//       <Sidebar /> {/* Include the Sidebar component */}
+import Navbar from './Navbar.js';
 import { Helmet } from 'react-helmet';
 
 import image1 from './images/5.jpg';
@@ -28,18 +27,13 @@ function App() {
         <meta name="description" content="21st Century Skill-Stacks for Everyone" />
         <link rel="icon" type="image/png" href="/favicon.ico" sizes="16x16" />
       </Helmet>
-      
-      <header className="Header">
+
+      <header className="header">
 
         <div className="LogoContainer">
           <img src={logo} alt="Logo" className="Logo" />
         </div>
-        <div className="header-left">
-          <button className="header-button">Home</button>
-          <button className="header-button">Products</button>
-          <button className="header-button">Locations</button>
-          <button className="header-button">About Us</button>
-        </div>
+        <Navbar />
       </header>
       <body>
         <div className="Section1">
